e705b950fad4870982f7faa486379cfc5648d656
Ishaan-Goel <139787489+Ishaan-Goel@users.noreply.github.com>
2023-12-31 15:45:28 -0800

    Add files via upload

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//  Created by Ishaan Goel on 12/24/2023.
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    var player: AVAudioPlayer!
+
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func Button(_ sender: UIButton) {
+        let ballArray = [#imageLiteral(resourceName: "02_no"), #imageLiteral(resourceName: "03_may_be"), #imageLiteral(resourceName: "05_hell_yeah"), #imageLiteral(resourceName: "06_hell_no"), #imageLiteral(resourceName: "07_really"), #imageLiteral(resourceName: "02_no"), #imageLiteral(resourceName: "01_yes")]
+        imageView.image = ballArray [Int.random(in: 0...6)]
+   //     playSound()
+        imageView.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [self] in
+            self.imageView.alpha = 1.0
+    }
+   // func playSound() {
+       //     let url = Bundle.main.url(forResource: "whoosh", withExtension: "wav")
+      //      player = try! AVAudioPlayer(contentsOf: url!)
+       //     player.play()
+
+    //}
+}
+}
